9a54a9b58f3e91934c59aa215b9fe700707b5077
Hunter Callaway <HunterCallaway@hotmail.com>
2019-02-08 00:13:36 -0700

    Created the private constant variables for the AdminTest file.

--- php/Classes/Test/AdminTest.php
+++ php/Classes/Test/AdminTest.php
@@ -13,10 +13,41 @@ require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
 /**
  * Full PHPUnit test for the Admin class
  *
- * This is a complete PHPUnit test of the Admin class. It is complete because all mySQL/PDO enabled methods
+ * This is a complete PHPUnit test of the Tweet class. It is complete because all mySQL/PDO enabled methods
  * are tested for both invalid and valid inputs.
  *
  * @see Admin
  * @author Hunter Callaway <jcallaway3@cnm.edu>
  *
  **/
+
+class AdminTest extends DataDesignTest {
+
+	/**
+	 * Valid e-mail address for the admin
+	 * @var VALID_ADMIN_EMAIL
+	 **/
+
+	private $VALID_ADMIN_EMAIL;
+
+	/**
+	 * Valid hash for the admin
+	 * @var VALID_ADMIN_HASH
+	 **/
+
+	private $VALID_ADMIN_HASH;
+
+	/**
+	 * Valid password for the admin
+	 * @var VALID_ADMIN_PASSWORD
+	 **/
+
+	private $VALID_ADMIN_PASSWORD;
+
+	/**
+	 * Valid username for the admin
+	 * @var VALID_ADMIN_USERNAME
+	 **/
+
+	private $VALID_ADMIN_USERNAME;
+}
