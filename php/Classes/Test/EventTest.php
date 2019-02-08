fa37e0c199e54453cb7dafa9665991d435539e00
Wyatt Salmons <wyattsalmons@gmail.com>
2019-02-08 09:10:04 -0700

    added constructor method https://app.asana.com/0/1108524870361544/list

--- php/Classes/Test/EventTest.php
+++ php/Classes/Test/EventTest.php
@@ -99,4 +99,51 @@ class Event implements \JsonSerializable {
 	 * @var string $eventVenueWebsite
 	 **/
 	private $eventVenueWebsite;
+
+	/**
+	 * constructor for Event
+	 *
+	 * @param string|Uuid $newEventId id of this event or null if a new event
+	 * @param string|Uuid $newEventAdminId id of the Admin that sent this Event
+	 * @param string $newEventAgeRequirement string contains age event requirements.
+	 * @param \DateTime|string $newEventDate date of the event
+	 * @param string $newEventDescription description of the event
+	 * @param \DateTime|string $newEventEndTime time event ends
+	 * @param string|Uuid $newEventImage Id of this event poster
+	 * @param string $newEventPrice price of the event
+	 * @param string $newEventPromoterWebsite Url of the promoter website
+	 * @param \DateTime|string $newEventStartTime time event starts
+	 * @param string $newEventTitle title of the event
+	 * @param string $newEventVenue name of venue hosting the event
+	 * @param string $newEventVenueWebsite Url of the venue website
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 * @Documentation https://php.net/manual/en/language.oop5.decon.php
+	 **/
+
+	public function __construct($newEventId, $newEventAdminId, string $newEventAgeRequirement, $newEventDate, string $newEventDescription, string $newEventEndTime, $newEventImage, string $newEventPrice, string $newEventPromoterWebsite, $newEventStartTime, string $newEventTitle, string $newEventVenue, string $newEventVenueWebsite) {
+		try {
+			$this->setEventId($newEventId);
+			$this->setEventAdminId($newEventAdminId);
+			$this->setEventAgeRequirements($newEventAgeRequirement);
+			$this->setEventDate($newEventDate);
+			$this->setEventDescription($newEventDescription);
+			$this->setEventEndTime($newEventEndTime);
+			$this->setEventImage($newEventImage);
+			$this->setEventPrice($newEventPrice);
+			$this->setEventPromoterWebsite($newEventPromoterWebsite);
+			$this->setEventStartTime($newEventStartTime);
+			$this->setEventTitle($newEventTitle);
+			$this->setEventVenue($newEventVenue);
+			$this->setEventVenueWebsite($newEventVenueWebsite);
+		}
+			//determine what exception type was thrown
+		catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+	}
+
 }
