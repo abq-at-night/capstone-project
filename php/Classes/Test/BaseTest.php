08ad98d9b8979ecb3eac596c598203fd24156f46
Wyatt Salmons <wyattsalmons@gmail.com>
2019-02-11 14:23:49 -0700

    renamed and edited from EventTest to BaseTest.php https://app.asana.com/0/1108524870361544/list

--- php/Classes/Test/BaseTest.php
+++ php/Classes/Test/BaseTest.php
@@ -47,10 +47,10 @@ abstract class AbqAtNightTest extends TestCase {
 
 		// add all the tables for the project here
 		// THESE TABLES *MUST* BE LISTED IN THE SAME ORDER THEY WERE CREATED!!!!
-		$dataset->addTable("profile");
-		$dataset->addTable("tweet");
-		// the second parameter is required because like is also a SQL keyword and is the only way PHPUnit can query the like table
-		$dataset->addTable("like", "SELECT likeProfileId, likeTweetId, likeDate FROM `like`");
+		$dataset->addTable("admin");
+		$dataset->addTable("event");
+		$dataset->addTable("eventTag");
+		$dataset->addTable("tag");
 		return($dataset);
 	}
 
