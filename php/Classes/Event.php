801ce7eec9859c2403ee81f788e58996c9bc3557
Wyatt Salmons <wyattsalmons@gmail.com>
2019-02-13 16:12:56 -0700

    changed namespace  https://app.asana.com/0/1108524870361544/list

--- php/Classes/Event.php
+++ php/Classes/Event.php
@@ -1,5 +1,5 @@
 <?php
-namespace DeepDive\AbqAtNight; /* change namespace to AbqAtNight/CapstoneProject */
+namespace AbqAtNight\CapstoneProject;
 
 require_once("autoload.php");
 require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
