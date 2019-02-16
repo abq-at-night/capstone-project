e50084bd473971ff37bc2fca12caede0772c317d
Hunter Callaway <HunterCallaway@hotmail.com>
2019-02-16 11:24:22 -0700

    Fixed the typos in the testGetAllTags() method. https://app.asana.com/0/1108524870361544/1108524870361547

--- php/Classes/Tests/TagTest.php
+++ php/Classes/Tests/TagTest.php
@@ -169,14 +169,13 @@ class TagTest extends AbqAtNightTest {
 		$results = Tag::getAllTags($this->getPDO());
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tag"));
 		$this->assertCount(1, $results);
-		$this->assertContainsOnlyInstancesOf("AbqAtNight\\CapstoneProject\\php\\Classes\\Tests\\Tag", $results);
+		$this->assertContainsOnlyInstancesOf("AbqAtNight\\CapstoneProject\\Tag", $results);
 
 		// grab the result from the array and validate it
 		$pdoTag = $results[0];
 		$this->assertEquals($pdoTag->getTagId(), $tagId);
-		$this->assertEquals($pdoTag->getTagId(), $this->admin->getAdminId());
+		$this->assertEquals($pdoTag->getTagAdminId(), $this->admin->getAdminId());
 		$this->assertEquals($pdoTag->getTagType(), $this->VALID_TAG_TYPE);
-		//format the date too seconds since the beginning of time to avoid round off error
 		$this->assertEquals($pdoTag->getTagValue(), $this->VALID_TAG_VALUE);
 	}
 }
