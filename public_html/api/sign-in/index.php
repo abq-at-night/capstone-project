812c6407e2099b91b18a021c926275d4b2fad186
Hunter Callaway <HunterCallaway@hotmail.com>
2019-02-27 18:24:17 -0700

    Created the session verification block, the empty reply, and began coding the try block. https://app.asana.com/0/1108524870361544/1111351170149589

--- public_html/api/sign-in/index.php
+++ public_html/api/sign-in/index.php
@@ -18,3 +18,27 @@ use AbqAtNight\CapstoneProject\{
  *
  * @author Hunter Callaway <jcallaway3@cnm.edu>
  **/
+
+//Verify the session. If it's not active, start it.
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//Prepare an empty reply.
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+
+	//Grab the mySQL connection.
+	$secrets = new \Secrets("/etc/apache2/capstone-mysql/atnight.ini");
+	$pdo = $secrets->getPdoObject();
+
+	//Determine which HTTP method was used.
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//If the method is POST, handle the sign-in logic.
+	if($method === "POST") {
+
+	}
+}
