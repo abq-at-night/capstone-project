a2a2917b7579a4693c4fa474f93cebb08cfb059d
Hunter Callaway <HunterCallaway@hotmail.com>
2019-03-02 15:18:55 -0700

    Updated the event object. https://app.asana.com/0/1108524870361544/1111351170149593

--- php/lib/event.php
+++ php/lib/event.php
@@ -1,10 +1,19 @@
 <?php
 
-namespace DeepDive\AbqAtNight;
+namespace AbqAtNight\CapstoneProject;
 // load the profile class
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
+use AbqAtNight\CapstoneProject\{Event, Admin};
 
+require_once(dirname(__DIR__, 1) . "/Classes/Admin.php");
 require_once(dirname(__DIR__, 1) . "/Classes/Event.php");
 
 //use the constructor to create a new author
-$test = new Event("e462fab1-e72d-4730-b9d8-3f7b43bd5ae7","570a137c-15e1-4c92-a657-e88e182eac2b", "21 and over", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "2019-02-12 22:00:00", "bf5a4324-0f9b-41d9-966a-8f820a1c03cc", "7 at the door", "promowebsite.com", "2019-02-12 19:00:00", "New Show", "Venue", "venuewebsite.com");
-var_dump($test);
+$secrets =  new \Secrets("/etc/apache2/capstone-mysql/cohort23/atnight.ini");
+$pdo = $secrets->getPdoObject();
+
+$event = new Event("2608d9a5-fe0f-4ed6-9574-0d9c3d016eca", "0e3bf5bf-722a-41eb-b742-42450522ff0e", "All ages", "Bringin' the rock.", "2019-03-31 02:00:00", "www.sweetpic.com", "35.084112", "-106.651193", "$25.00", "www.mypromotersite.com", "2019-03-30 21:00:00", "Rock ABQ presents Hinder and Trapt", "Sunshine Theater", "www.sunshinetheaterlive.com");
+$event->insert($pdo);
+var_dump($event);
+$newEvent = Event::getEventByEventId($pdo, $event->getEventId());
+var_dump($newEvent);
