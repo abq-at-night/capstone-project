07420761fcbea4d0ab9cdf2b9e0376ae89967d34
Wyatt Salmons <wyattsalmons@gmail.com>
2019-03-09 15:37:52 -0700

    updated static ui header, navbar  https://app.asana.com/0/1108524870361544/list

--- static-ui/navbar.php
+++ static-ui/navbar.php
@@ -1,15 +1,11 @@
 <?php require_once("head-utils.php"); ?>
 
 <main>
-	<div class="container">
-		<div class="row">
-			<nav class="navbar navbar-dark bg-dark">
-				<a class="navbar-brand"><img src="../src/img/abq-at-night-logo-white.png" alt="logo white" id="logo"></a>
-				<form class="form-inline">
-					<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-				</form>
-			</nav>
-		</div>
-	</div>
+	<nav class="navbar navbar-dark bg-dark">
+		<img class="mx-auto pb-4" src="../src/img/abq-at-night-logo-white.png" alt="logo white">
+		<form class="form-inline" id="searchbar">
+			<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+			<button class="btn btn-outline-success my-2" type="submit">Search</button>
+		</form>
+	</nav>
 </main>
