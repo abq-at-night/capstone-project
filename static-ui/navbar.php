28a0c2cf6fa24f3307fbb7316a08302505322c7d
Wyatt Salmons <wyattsalmons@gmail.com>
2019-03-09 15:01:13 -0700

    added static ui header, navbar, logo, and file directory for all  ,  https://app.asana.com/0/1108524870361544/list

---
+++ static-ui/navbar.php
@@ -0,0 +1,15 @@
+<?php require_once("head-utils.php"); ?>
+
+<main>
+	<div class="container">
+		<div class="row">
+			<nav class="navbar navbar-dark bg-dark">
+				<a class="navbar-brand"><img src="../src/img/abq-at-night-logo-white.png" alt="logo white" id="logo"></a>
+				<form class="form-inline">
+					<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+				</form>
+			</nav>
+		</div>
+	</div>
+</main>
