f09d61354c316cc532ad62d73c8a9487fa7a8b46
Wyatt Salmons <wyattsalmons@gmail.com>
2019-03-11 13:11:54 -0600

    rolled back to last good push https://app.asana.com/0/1108524870361544/list

--- static-ui/navbar.php
+++ static-ui/navbar.php
@@ -1,11 +1,38 @@
 <?php require_once("head-utils.php"); ?>
 
 <main>
-	<nav class="navbar navbar-dark bg-dark">
-		<img class="mx-auto pb-4" src="../src/img/abq-at-night-logo-white.png" alt="logo white">
+	<nav class="navbar navbar-dark">
+		<img class="pb-2" src="../src/img/abq-at-night-logo-white.png" alt="logo white">
 		<form class="form-inline" id="searchbar">
+			<button class="navbar-toggler pb-3" type="button" data-toggle="collapse"
+					  data-target="#navbarSupportedContent-7" aria-controls="navbarSupportedContent-7"
+					  aria-expanded="false" aria-label="Toggle navigation">
+				<div class="navbar-toggler-icon"></div>
+			</button>
 			<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-			<button class="btn btn-outline-success my-2" type="submit">Search</button>
+
+
+
+
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent-7">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<input class="form-control mr-sm-2" type="search" placeholder="Genre" aria-label="Genre">
+					</li>
+					<li class="nav-item">
+						<input class="form-control mr-sm-2" type="search" placeholder="Venue" aria-label="Venue">
+					</li>
+					<li class="nav-item">
+						<input class="form-control mr-sm-2" type="search" placeholder="Date" aria-label="Date">
+					</li>
+				</ul>
+
+
+
+
+
+			<button class="btn btn-outline-danger my-2" type="submit">Search</button>
 		</form>
 	</nav>
 </main>
