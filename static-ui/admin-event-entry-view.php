a5a0e16c8e0f90f55046eb56d5e6b8cc401877e8
Hunter Callaway <HunterCallaway@hotmail.com>
2019-03-11 16:18:39 -0600

    Centering the form labels and inputs. https://app.asana.com/0/1108524870361544/1111351170149593

--- static-ui/admin-event-entry-view.php
+++ static-ui/admin-event-entry-view.php
@@ -8,95 +8,93 @@
 
 <div class="container">
 
-	<div id="event-entry-form" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+	<div id="event-entry-form" class="mainbox col d-flex justify-content-center">
 		<div class="panel panel-info">
 			<div class="panel-heading">
-				<div class="panel-title">Create a new event.</div>
+				<div class="h4 panel-title mb-2 mt-2">Create a new event.</div>
 			</div>
-			<div class="panel-body">
-				<form method="post" action=".">
 
 
 					<div id="event-image" class="form-group required">
-						<label for="event-image" class="control-label col-md-4 requiredField">Image<span class="asteriskField">*</span> </label>
-						<div class="controls col-md-8 ">
-							<input class="input-md textinput textInput form-control" id="event-image" maxlength="30" name="event-image" placeholder="www.eventimageurl.com" style="margin-bottom: 10px" type="text" />
+						<label for="event-image" class="control-label requiredField">Image<span class="asteriskField">*</span> </label>
+						<div class="controls">
+							<input class="input-md textinput textInput form-control mb-1" id="event-image" name="event-image" placeholder="www.eventimageurl.com" type="text" />
 						</div>
 					</div>
 
 						<div id="event-title" class="form-group required">
 							<label for="event-title" class="control-label col-md-4  requiredField">Title<span class="asteriskField">*</span> </label>
-							<div class="controls col-md-8 ">
-								<input class="input-md textinput textInput form-control" id="event-title" maxlength="30" name="event-title" placeholder="Enter a title." style="margin-bottom: 10px" type="text" />
+							<div class="controls">
+								<input class="input-md textinput textInput form-control mb-1" id="event-title" name="event-title" placeholder="Enter a title." type="text" />
 							</div>
 						</div>
 
 					<div id="event-venue" class="form-group">
 						<label for="event-venue" class="control-label col-md-4">Venue</label>
-						<div class="controls col-md-8 ">
-							<input class="input-md textinput textInput form-control" id="event-venue" name="event-venue" placeholder="Enter a venue. (optional)" style="margin-bottom: 10px" type="text" />
+						<div class="controls">
+							<input class="input-md textinput textInput form-control mb-1" id="event-venue" name="event-venue" placeholder="Enter a venue. (optional)" type="text" />
 						</div>
 					</div>
 
 					<div id="event-latitude" class="form-group">
 						<label for="event-latitude" class="control-label col-md-4">Latitude</label>
-						<div class="controls col-md-8 ">
+						<div class="controls">
 							<input class="input-md textinput textInput form-control mb-1" id="event-latitude" name="event-latitude" placeholder="BE CAREFUL! Only six digits are allowed after the decimal." type="text" />
 						</div>
 					</div>
 
 					<div id="event-longitude" class="form-group">
 						<label for="event-longitude" class="control-label col-md-4">Longitude</label>
-						<div class="controls col-md-8 ">
+						<div class="controls">
 							<input class="input-md textinput textInput form-control mb-1" id="event-longitude" name="event-longitude" placeholder="BE CAREFUL! Only six digits are allowed after the decimal." type="text" />
 						</div>
 					</div>
 
 					<div id="event-date-start-time" class="form-group required">
-						<label for="event-date-start-time" class="control-label col-md-4  requiredField">Start Date and Start Time<span class="asteriskField">*</span> </label>
-						<div class="controls col-md-8 ">
+						<label for="event-date-start-time" class="control-label requiredField">Start Date and Start Time<span class="asteriskField">*</span> </label>
+						<div class="controls">
 							<input class="input-md textinput textInput form-control mb-1" id="event-date-start-time" name="event-date-start-time" placeholder="Y-M-D H:M:S" type="text" />
 						</div>
 					</div>
 
 					<div id="event-date-end-time" class="form-group required">
-						<label for="event-date-end-time" class="control-label col-md-4  requiredField">End Date and End Time<span class="asteriskField">*</span> </label>
-						<div class="controls col-md-8 ">
+						<label for="event-date-end-time" class="control-label requiredField">End Date and End Time<span class="asteriskField">*</span> </label>
+						<div class="controls">
 							<input class="input-md textinput textInput form-control mb-1" id="event-date-end-time" name="event-date-end-time" placeholder="Y-M-D H:M:S" type="text" />
 						</div>
 					</div>
 
 					<div id="event-price" class="form-group">
 						<label for="event-price" class="control-label col-md-4">Price</label>
-						<div class="controls col-md-8 ">
+						<div class="controls">
 							<input class="input-md textinput textInput form-control mb-1" id="event-price" name="event-price" placeholder="Enter the price. (optional)" type="text" />
 						</div>
 					</div>
 
 					<div id="event-age-requirement" class="form-group">
-						<label for="event-age-requirement" class="control-label col-md-4">Age Requirement</label>
-						<div class="controls col-md-8 ">
+						<label for="event-age-requirement" class="control-label">Age Requirement</label>
+						<div class="controls">
 							<input class="input-md textinput textInput form-control mb-1" id="event-age-requirement" name="event-age-requirement" placeholder="Enter the age requirement. (optional)" type="text" />
 						</div>
 					</div>
 
 						<div id="event-description" class="form-group required">
-							<label for="event-description" class="control-label col-md-4  requiredField">Event Description<span class="asteriskField">*</span> </label>
-							<div class="controls col-md-8 ">
+							<label for="event-description" class="control-label requiredField">Event Description<span class="asteriskField">*</span> </label>
+							<div class="controls">
 								<input class="input-md textinput form-control mb-1" id="event-description" name="event-description" placeholder="Enter an event description." type="text" />
 							</div>
 						</div>
 
 						<div id="event-venue-website" class="form-group">
-							<label for="event-venue-website" class="control-label col-md-4">Venue Website</label>
-							<div class="controls col-md-8 ">
+							<label for="event-venue-website" class="control-label">Venue Website</label>
+							<div class="controls">
 								<input class="input-md textinput textInput form-control mb-1" id="event-venue-website" name="event-venue-website" placeholder="www.eventvenue.com (optional)" type="text" />
 							</div>
 						</div>
 
 					<div id="event-promoter-website" class="form-group">
-						<label for="event-promoter-website-website" class="control-label col-md-4">Promoter Website</label>
-						<div class="controls col-md-8 ">
+						<label for="event-promoter-website-website" class="control-label">Promoter Website</label>
+						<div class="controls">
 							<input class="input-md textinput textInput form-control mb-1" id="event-promoter-website" name="event-promoter-website" placeholder="www.promoterwebsite.com (optional)" type="text" />
 						</div>
 					</div>
@@ -104,12 +102,11 @@
 			</div>
 		</div>
 
-						<div class="form-group">
-							<div class="aab controls col-md-4 "></div>
-							<div class="controls col-md-8 ">
+						<div class="form-group d-flex justify-content-center">
+							<div class="aab controls"></div>
+							<div class="controls">
 								<input type="submit" name="Submit" value="Submit" class="btn btn-primary btn btn-info" id="submit" />
 							</div>
 						</div>
 
-	</div>
 </div>
