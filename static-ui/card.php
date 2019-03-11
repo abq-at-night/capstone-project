652f51d54b6972cf1c5ff350704c6e0fa097695e
Adian Tsosie <addve12@yahoo.com>
2019-03-11 15:48:13 -0600

    fixed margin, padding and centered items in drop down. https://app.asana.com/0/1108524870361544/list

--- static-ui/card.php
+++ static-ui/card.php
@@ -4,35 +4,41 @@
 
 <body>
 <div id="accordion">
-    <div class="card my-5 mx-auto" style="max-width: 500px;">
+    <div class="card my-3 mx-auto" style="max-width: 500px;">
         <div class="row justify-content-center">
-            <div class="col-sm-2 my-auto genre">
-                <p class="mx-0"><strong>EDM</strong></p>
+            <div class="col-sm-1 my-auto px-0 text-center genre">
+                <p class="mx-0 mb-3"><strong>EDM</strong></p>
             </div>
-            <div class="col-sm-4">
-                <img src="img/bonfire-3.jpg" class="card-img-fluid rounded my-3 px-0" alt="...">
+            <div class="col-sm-3">
+                <img src="img/bonfire-3.jpg" class="card-img-fluid rounded my-3 pl-0" alt="...">
             </div>
-            <div class="col-sm-6">
+            <div class="col-sm-8">
                 <div class="card-body">
                     <h5 class="card-title">Bonfire</h5>
                     <p class="card-text my-0">ABQue the Music brings you a night of Music under the stars</p>
                     <p class="card-text venue my-0"><strong>Open Space Abq</strong></p>
                     <p class="card-text my-0">7-29-2019 </p>
+                    <p class="card-text my-0">9:00pm - 2:00am </p>
                     <p class="card-text my-0">$10 Cover</p>
                 </div>
             </div>
-            <div class="row">
-                <div>
-                    <button class="btn my-1 justify-content-center" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne"><i class="fas fa-info"></i>
-                    More Info</button>
+            <div class="row justify-content-center">
+                <div class="col-sm mt-1">
+                    <button class="btn mb-2" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne"><i class="fas fa-info"></i>
+                        More Info</button>
                 </div>
-                <div id="collapseOne" class="collapse justify-content-center" aria-labelledby="headingOne" data-parent="#accordion">
-                    <div class="card-body">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-                        3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
-                        laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua sustainable VHS.
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-sm-10 mb-4">
+                    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                        <div class="card-body-sm-6 mx-auto">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                            3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
+                            laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua sustainable VHS.
+                        </div>
                     </div>
                 </div>
+            </div>
             </div>
         </div>
     </div>
