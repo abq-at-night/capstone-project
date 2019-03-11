5254ed71a24fc10b04942992c29366771c918ceb
Wyatt Salmons <wyattsalmons@gmail.com>
2019-03-11 12:59:29 -0600

    Revert "fixed positioning of navbar elements and made them responsive. reverted to avoid icky styling, post decision to avoid multiple search bars.    https://app.asana.com/0/1108524870361544/list"
    This reverts commit c93e30b8

--- static-ui/navbar.php
+++ static-ui/navbar.php
@@ -1,14 +1,11 @@
 <?php require_once("head-utils.php"); ?>
 
 <main>
-	<nav class="navbar navbar-dark">
-		<img class="pb-2" src="../src/img/abq-at-night-logo-white.png" alt="logo white">
+	<nav class="navbar navbar-dark bg-dark">
+		<img class="mx-auto pb-4" src="../src/img/abq-at-night-logo-white.png" alt="logo white">
 		<form class="form-inline" id="searchbar">
 			<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-			<input class="form-control mr-sm-2" type="search" placeholder="Genre" aria-label="Genre">
-			<input class="form-control mr-sm-2" type="search" placeholder="Venue" aria-label="Venue">
-			<input class="form-control mr-sm-2" type="search" placeholder="Date" aria-label="Date">
-			<button class="btn btn-outline-danger my-2" type="submit">Search</button>
+			<button class="btn btn-outline-success my-2" type="submit">Search</button>
 		</form>
 	</nav>
 </main>
