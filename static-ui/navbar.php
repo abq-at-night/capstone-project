c93e30b864cf29cc639808512f1ce516e5a9fca7
Wyatt Salmons <wyattsalmons@gmail.com>
2019-03-11 10:47:21 -0600

    fixed positioning of navbar elements and made them responsive.   https://app.asana.com/0/1108524870361544/list

--- static-ui/navbar.php
+++ static-ui/navbar.php
@@ -2,7 +2,7 @@
 
 <main>
 	<nav class="navbar navbar-dark bg-dark">
-		<img class="mx-auto pb-4" src="../src/img/abq-at-night-logo-white.png" alt="logo white">
+		<img class="pb-2" src="../src/img/abq-at-night-logo-white.png" alt="logo white">
 		<form class="form-inline" id="searchbar">
 			<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
 			<button class="btn btn-outline-success my-2" type="submit">Search</button>
