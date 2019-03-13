1e659c491d21c9ed15f1c0b4589bf7366b9ee7b7
Wyatt Salmons <wyattsalmons@gmail.com>
2019-03-13 14:58:11 -0600

    wrote event interface.  https://app.asana.com/0/1108524870361544/list

---
+++ ng/src/app/shared/services/interfaces/event.ts
@@ -0,0 +1,14 @@
+export interface Event {
+	eventAgeRequirement: string,
+	eventDescription: string,
+	eventEndTime: string,
+	eventImage:string,
+	eventLat: number,
+	eventLng: number,
+	eventPrice: string,
+	eventStartTime: string,
+	eventTitle: string,
+	eventPromoterWebsite: string,
+	eventVenue: string,
+	eventVenueWebsite: string
+}
