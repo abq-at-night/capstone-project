b014bc24404ff5ae40afde71d945941a26ef4be5
Hunter Callaway <HunterCallaway@hotmail.com>
2019-03-13 23:10:11 -0600

    Changed the admin sign-in modal to a form. https://app.asana.com/0/1108524870361544/1111351170149593

--- static-ui/admin-sign-in-modal.php
+++ static-ui/admin-sign-in-modal.php
@@ -4,28 +4,23 @@
 
 <main>
 
-	<! Modal -->
-	<div class="modal fade" id="admin-sign-in" tabindex="-1" role="dialog" aria-labelledby="admin-sign-in" aria-hidden="true">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content gray">
-				<div class="modal-header gray">
-					<h5 class="modal-title" id="admin-sign-in">Admin Sign-in</h5>
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
-				</div>
-				<div class="modal-body gray">
-					<form class="form-control lg" id="form" action="" method="post">
-						<div class="info">
-							<input class="form-control" id="username" type="text" name="username" placeholder=" Username" />
-							<input class="form-control" id="password" type="text" name="password" placeholder=" Password" />
-						</div>
-					</form>
-					<div class="modal-footer">
-						<input class="btn" type="submit" value="Sign In">
-					</div>
-				</div>
+	<form>
+		<div class="form-group mx-5 mt-2">
+			<label for="username">Username</label>
+			<input type="email" class="form-control" id="username" aria-describedby="usernameHelp" placeholder="Enter your username.">
+		</div>
+
+		<div class="form-group mx-5">
+			<label for="password">Password</label>
+			<input type="password" class="form-control" id="password1" placeholder="Password">
+		</div>
+
+		<div class="form-group d-flex justify-content-center">
+			<div class="aab controls"></div>
+			<div class="controls">
+				<input type="submit" name="Submit" value="Submit" class="btn btn-danger btn btn-info" id="submit" />
 			</div>
 		</div>
-	</div>
+	</form>
+
 </main>
