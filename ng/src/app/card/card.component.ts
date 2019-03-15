634b0c86f90867f9ab190e94095080791af52c90
Hunter Callaway <HunterCallaway@hotmail.com>
2019-03-15 09:01:35 -0600

    Updated the card.component.ts file. https://app.asana.com/0/1108524870361544/1111351170149593

--- ng/src/app/card/card.component.ts
+++ ng/src/app/card/card.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
 
-import {SessionService} from "../shared/services/session.service";
-
 import {Status} from "../shared/interfaces/status";
 import {Event} from "../shared/interfaces/event";
 import {EventService} from "../shared/services/event.service";
@@ -14,47 +12,22 @@ import {EventService} from "../shared/services/event.service";
 
 export class EventComponent implements OnInit {
 
-	event: Event = {
-		eventId: null,
-		eventAdminId: null,
-		eventAgeRequirement: null,
-		eventDescription: null,
-		eventEndTime: null,
-		eventImage: null,
-		eventLat: null,
-		eventLng: null,
-		eventPrice: null,
-		eventStartTime: null,
-		eventTitle: null,
-		eventPromoterWebsite: null,
-		eventVenue: null,
-		eventVenueWebsite: null};
+	events: Event[] = [];
 	status: Status = null;
 
 	constructor(
 		private router: Router,
 		private eventService: EventService,
-		private sessionService: SessionService
 	){}
 
 	ngOnInit(): void {
-		this.listCards();
+		this.loadCards();
 	}
 
-	listCards() : any {
+	loadCards() {
 
-		this.eventService.getEvent(this.event.eventId)
-			.subscribe(status => {
-				this.status = status;
-
-				if (this.status.status === 200) {
-					this.sessionService.setSession();
-					location.reload();
-
-					this.router.navigate(["/card"])
-				}
-			})
-	}
-
-	//TODO create methods for the other methods listed in the service file--but how?
+		this.eventService.getAllEvents()
+			.subscribe(reply =>
+				this.events = reply)
+			}
 }
