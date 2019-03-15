283350a5d3d813f2afebf3629d25e00c8dade8ce
Wyatt Salmons <wyattsalmons@gmail.com>
2019-03-15 09:32:09 -0600

    cont. event card component   https://app.asana.com/0/1108524870361544/list

--- static-ui/card.php
+++ static-ui/card.php
@@ -3,34 +3,46 @@
 <?php require_once("navbar.php"); ?>
 
 <div class="container">
-    <div id="accordion">
-        <div class="card mx-auto my-4">
-            <div class="row justify-content-center">
-                <div class="col-4 ml-3 my-auto py-3 pr-0">
-                    <img src="img/bonfire-3.jpg" class="card-img rounded" alt="Bonfire">
-                </div>
-                <div class="col">
-                    <div class="card-body pl-0">
-                        <h5 class="card-title m-0">Bonfire</h5>
-                        <p class="card-text venue my-0"><strong>Open Space Abq</strong></p>
-                        <p class="card-text my-0">5-20-2016 </p>
-                        <p class="card-text my-0">9:00pm - 2:00am </p>
-                        <p class="card-text my-0">$10 Cover</p>
-                        <button class="btn px-0" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne"><i class="fas fa-info-circle"></i></button>
-                    </div>
-                </div>
-                <div class="row-7 justify-content-center">
-                    <div class="col mt-0">
-                        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-                            <div class="card-body">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-                                3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
-                                laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua sustainable VHS.
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div id="accordion">
+		<div class="card mx-auto my-4 shadow">
+			<div class="row justify-content-center">
+				<div class="col-4 ml-3 my-auto py-3 pr-0">
+					<img src="img/bonfire-3.jpg" class="card-img rounded" alt="Bonfire">
+				</div>
+				<div class="col">
+					<div class="card-body pl-0">
+						<h5 class="card-title m-0">Bonfire</h5>
+						<p class="card-text venue my-0"><strong>Open Space Abq</strong></p>
+						<p class="card-text my-0">5-20-2016 </p>
+						<p class="card-text my-0">9:00pm - 2:00am </p>
+						<p class="card-text my-0">$10 Cover</p>
+						<button class="btn px-0" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false"
+								  aria-controls="collapseOne"><i class="fas fa-info-circle"></i></button>
+					</div>
+				</div>
+				<div class="row-7 justify-content-center">
+					<div class="col mt-0">
+						<div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+							<div class="card-body pt-0">
+								<hr class="pb-2">
+								<p>
+								Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+								3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
+								laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua sustainable VHS.
+								</p>
+								<a href="venuewebsite.come">venuewebsite.com</a> <br>
+								<a href="promowebsite.com">promowebsite.com</a> <br>
+								<span class="badge badge-pill badge-danger mt-2">eventTags</span>
+								<span class="badge badge-pill badge-primary mt-2">eventTags</span>
+								<span class="badge badge-pill badge-primary mt-2">eventTags</span>
+								<span class="badge badge-pill badge-primary mt-2">eventTags</span>
+								<span class="badge badge-pill badge-primary mt-2">eventTags</span>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
