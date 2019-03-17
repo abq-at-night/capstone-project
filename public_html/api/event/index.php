79e86e8d2072846d15b05633f6ac6dcd7df03487
Wyatt Salmons <wyattsalmons@gmail.com>
2019-03-17 11:51:23 -0600

    fixed error in event api https://app.asana.com/0/1108524870361544/list

--- public_html/api/event/index.php
+++ public_html/api/event/index.php
@@ -123,7 +123,7 @@ try {
 		// make sure Event end date is accurate
 		if(empty($requestObject->eventEndTime) === true) {
 			throw (new InvalidArgumentException("Event end time must be inserted.", 405));
-		} $eventEndTime = DateTime::createFromFormat("U.u", $requestObject->eventStartTime / 1000);
+		} $eventEndTime = DateTime::createFromFormat("U.u", $requestObject->eventEndTime / 1000);
 		if($eventEndTime === false) {
 			throw(new RuntimeException("Invalid event date", 400));
 		} else {
