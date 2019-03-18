02d64e130534c6b40a5104a138f56ff5bf83664d
Wyatt Salmons <wyattsalmons@gmail.com>
2019-03-18 11:45:45 -0600

    added navbar component files and included datepicker to nav static. https://app.asana.com/0/1108524870361544/list

--- static-ui/navbar.php
+++ static-ui/navbar.php
@@ -3,9 +3,24 @@
 <main>
 	<nav class="navbar navbar-dark">
 		<img class="pb-2" src="../src/img/abq-at-night-logo-white.png" alt="logo white">
-		<form class="form-inline" id="searchbar">
-			<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-			<button class="btn btn-outline-danger my-2" type="submit">Search</button>
+		<!--form-->
+		<form class="form-group row no-gutters" id="searchbar">
+			<!--input-->
+			<div class="col">
+				<input class="form-control d-inline-block" type="search" placeholder="Search" aria-label="Search">
+			</div>
+			<!--button-->
+			<div class="col-sm-2">
+				<span><button class="btn btn-outline-danger ml-sm-2" type="submit">Search</button></span>
+			</div>
+			<!--input-->
+			<div class="col">
+				<input class="form-control d-inline-block" type="date" placeholder="Search" aria-label="Search">
+			</div>
+			<!--button-->
+			<div class="col-sm-2">
+				<span><button class="btn btn-outline-danger ml-sm-2" type="submit">Search</button></span>
+			</div>
 		</form>
 	</nav>
 </main>
