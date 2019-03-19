3e327eebcf729692a1ecbe6d55f7092a50a0c8cf
Hunter Callaway <HunterCallaway@hotmail.com>
2019-03-19 08:35:22 -0600

    Added the code for the datetime-picker.component.ts file. https://app.asana.com/0/1108524870361544/1111351170149593

---
+++ ng/src/app/datetime-picker/datetime-picker.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from "@angular/core";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+	templateUrl: "datetime-picker.component.html"
+})
+
+export class DatetimePickerComponent implements OnInit {
+
+	dateTimePickerForm: FormGroup = null;
+
+	constructor(private formBuilder: FormBuilder) {
+
+	}
+
+	ngOnInit(): void {
+		this.dateTimePickerForm = this.formBuilder.group({
+			date: ["", [Validators.required]],
+			time: ["", [Validators.required]]
+		})
+	}
+
+	submit() {
+		let date = this.dateTimePickerForm.value.date;
+		let time = this.dateTimePickerForm.value.time;
+		let startTime = new Date(date.year, date.month, date.day, time.hour, time.minute);
+		console.log(startTime.getTime());
+	}
+}
